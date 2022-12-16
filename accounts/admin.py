a88995bb180c54991adde2a7eb7f7cebfb273951
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-12-16 16:14:38 +0545

    feat: customize admin pannel manually with profile inlines

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
+from accounts.models import Profile
+from accounts.forms import CustomUserChangeForm, CustomUserCreationForm
 # Register your models here.
+
+
+User = get_user_model()
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username','email','email_verified','is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional Info",{
+                "fields":("gender","email_verified","date_of_birth"),
+            }
+        ),
+    )
+    add_fieldsets = (
+        (
+            "Create User",{
+                "classes":("wide",),
+                "fields":("username","email","password1","password2"),
+            }
+        ),
+    )
+    def get_inlines(self, request, obj=None):
+        if obj:
+            return [ProfileInline]
+        return []
