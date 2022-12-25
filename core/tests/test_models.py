c8ff8e69adbbbff168c025f4902d0f59d8db0288
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-12-25 20:55:01 +0545

    feat: create 3 unit test for custom model

---
+++ core/tests/test_models.py
@@ -0,0 +1,46 @@
+"""
+Tests for models
+"""
+
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class ModelTests(TestCase):
+    """Test Models"""
+    def test_create_user_with_email_successfull(self):
+        """Testing creating a user with an email is successfull"""
+        email = "testing@gmail.com"
+        password = "testpass@123"
+        username = "test"
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        self.assertEqual(user.username, username)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1','test1@EXAMPLE.com','test1@example.com'],
+            ['test2','Test2@Example.COM','Test2@example.com'],
+            ['test3','TEST3@EXAMPLE.COM','TEST3@example.com'],
+            ['test4','test4@example.COM','test4@example.com'],
+        ]
+
+        for username, email, expected in sample_emails:
+            user = User.objects.create_user(username=username, email=email, password="hello123")
+            self.assertEqual(user.email, expected)
+    
+    def test_new_user_without_username_raises_error(self):
+        """Test creating new user without username will raises ValueError"""
+        with self.assertRaises(ValueError):
+            User.objects.create_user(username="",email="admin212@asd.com",password="hello@123")
+            
